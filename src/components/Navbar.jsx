967aae4a62873deaceddc7883d758738b4428eee
didiraja <didiraja@hotmail.com>
2023-04-09 18:16:38 -0300

    style: 💄 navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,26 @@
 import Image from 'next/image';
+import Button from './Button';
 import styles from './Navbar.module.scss';
+
+function MenuDesktop() {
+  return (
+    <div className={`${styles.menu} hidden lg:flex w-[70%]`}>
+      <ul className="w-full flex justify-between items-center menu-list">
+        <li>Menu 1</li>
+        <li>Menu 2</li>
+        <li>Menu 3</li>
+        <li>Menu 4</li>
+        <li>Menu 5</li>
+
+        <div className="btn-wrapper">
+          <Button secondary>Button</Button>
+          <Button>Button</Button>
+        </div>
+      </ul>
+
+    </div>
+  );
+}
 
 function Navbar() {
   return (
@@ -12,13 +33,15 @@ function Navbar() {
         height={24}
         priority
       />
-
       <Image
         src="/icon/menu.svg"
+        className="lg:hidden"
         alt="menu"
         width={32}
         height={32}
       />
+
+      <MenuDesktop />
     </navbar>
   );
 }
