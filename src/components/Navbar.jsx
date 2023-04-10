fbe5c1a2cf43899de9d45088844a0fdcd8c826df
didiraja <didiraja@hotmail.com>
2023-04-10 14:59:28 -0300

    refactor: 💡 started Navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,40 @@
 import Image from 'next/image';
+import { useMediaQuery, useToggle } from 'usehooks-ts';
+
 import Button from './Button';
 import styles from './Navbar.module.scss';
 
+function NavCTA() {
+  return (
+    <div className={`${styles.ctaBlock}`}>
+      <div className="btn-wrapper">
+        <Button secondary>Button</Button>
+        <Button>Button</Button>
+      </div>
+    </div>
+  );
+}
+
+function MenuMobile() {
+  return (
+    <div className={`${styles.menuMobile}`}>
+      <ul className="w-full menu-list">
+        <li>Menu 1</li>
+        <li>Menu 2</li>
+        <li>Menu 3</li>
+        <li>Menu 4</li>
+        <li>Menu 5</li>
+
+      </ul>
+
+      <NavCTA />
+    </div>
+  );
+}
+
 function MenuDesktop() {
   return (
-    <div className={`${styles.menu} hidden lg:flex w-[70%]`}>
+    <div className={`${styles.menuDesktop} hidden lg:flex w-[70%]`}>
       <ul className="w-full flex justify-between items-center menu-list">
         <li>Menu 1</li>
         <li>Menu 2</li>
@@ -12,10 +42,7 @@ function MenuDesktop() {
         <li>Menu 4</li>
         <li>Menu 5</li>
 
-        <div className="btn-wrapper">
-          <Button secondary>Button</Button>
-          <Button>Button</Button>
-        </div>
+        <NavCTA />
       </ul>
 
     </div>
@@ -23,25 +50,50 @@ function MenuDesktop() {
 }
 
 function Navbar() {
+  const [showMenu, toggleMenu, setMenu] = useToggle();
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
+
   return (
     <navbar className={styles.navbar}>
+
       <Image
         src="/logo-default.svg"
         alt="RD Station"
-          // className={}
         width={150}
         height={24}
         priority
       />
-      <Image
-        src="/icon/menu.svg"
-        className="lg:hidden"
-        alt="menu"
-        width={32}
-        height={32}
-      />
 
-      <MenuDesktop />
+      {
+        showMenu ? (
+          <Image
+            src="/icon/close.svg"
+            alt="menu"
+            width={32}
+            height={32}
+            onClick={toggleMenu}
+          />
+        ) : null
+      }
+
+      {
+        !showMenu ? (
+          <Image
+            src="/icon/menu.svg"
+            alt="menu"
+            width={32}
+            height={32}
+            onClick={toggleMenu}
+          />
+        ) : null
+      }
+
+      {
+        showMenu ? (
+          <MenuMobile />
+        ) : null
+      }
+
     </navbar>
   );
 }
