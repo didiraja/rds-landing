e738952a029ce658799c5b80ba708b3ac29279a6
didiraja <didiraja@hotmail.com>
2023-04-13 12:03:36 -0300

    refactor: 💡 CTA e Demonstration done

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,19 +4,21 @@ import styles from './CTA.module.scss';
 function Benefits() {
   return (
     <section className={styles.cta}>
-      <div className={styles.container}>
-        <p className="title">
-          Por que mais de 25.000 empresas escolheram o RD Station?
-        </p>
-        <p className="content">
-          Combinamos nossos produtos de Marketing e Vendas com pessoas que
-          se importam com seus resultados e um ecossistema que apoia o seu
-          negócio do planejamento à prática.
-        </p>
+      <div className="container">
+        <div className="wrapper">
+          <p className="title">
+            Por que mais de 25.000 empresas escolheram o RD Station?
+          </p>
+          <p className="content">
+            Combinamos nossos produtos de Marketing e Vendas com pessoas que
+            se importam com seus resultados e um ecossistema que apoia o seu
+            negócio do planejamento à prática.
+          </p>
 
-        <Button highlight className="w-full">
-          criar conta gratuita
-        </Button>
+          <Button highlight className="mx-auto">
+            criar conta gratuita
+          </Button>
+        </div>
       </div>
     </section>
   );
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,12 +5,12 @@ import styles from './Demo.module.scss';
 function Demo() {
   return (
     <section className={styles.demo}>
-      <div className={styles.container}>
+      <div className="container mx-auto">
         <p className="title">
           Entenda melhor como o RD Station Marketing funciona na prática
         </p>
 
-        <Button secondary className="w-full button">
+        <Button secondary className="button mx-auto">
           assista a demonstração
           {' '}
           {'>'}
